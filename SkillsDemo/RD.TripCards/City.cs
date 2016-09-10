671a36c719174aeb814a7e545bd2569b3ac0b372
Roman <r.dyagel@gmail.com>
2016-09-10 12:04:26 +0400

    Карточки путешестивий. Добавил проект

---
+++ SkillsDemo/RD.TripCards/City.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RD.TripCards
+{
+    public sealed class City : IEquatable<City>
+    {
+        public City(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+                throw new ArgumentException("name");
+
+            Name = name;
+        }
+
+        public string Name
+        {
+            get;
+            private set;
+        }
+
+        public bool Equals(City other)
+        {
+            if (other == null)
+                return false;
+
+            return string.Equals(Name, other.Name);
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as City;
+            if (other != null)
+                return Equals(other);
+
+            return false;
+        }
+    }
+}
---
+++ SkillsDemo/RD.TripCards/TripCard.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RD.TripCards
+{
+    public sealed class TripCard
+    {
+        public TripCard(City from, City to)
+        {
+            Validate(from, to);
+
+            From = from;
+            To = to;
+        }
+
+        private void Validate(City from, City to)
+        {
+            if (from == null)
+                throw new ArgumentNullException("from");
+            if (to == null)
+                throw new ArgumentNullException("to");
+            if (object.Equals(from, to))
+                throw new ArgumentException("Cities must be not equals");
+        }
+
+        public City From
+        {
+            get;
+            private set;
+        }
+
+        public City To
+        {
+            get;
+            private set;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}->{1}", From, To);
+        }
+    }
+}
